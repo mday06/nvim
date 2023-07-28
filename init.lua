12bc00b5db167727ce3e4c1750b1416204c01205
mday <mday@thinkpad.OpenBSD>
2023-07-29 02:03:06 +0300

    Neovim

---
+++ init.lua
@@ -0,0 +1,9 @@
+require "plugins"
+require "user.options"
+require "user.colorscheme"
+require("nvim-tree").setup()
+
+vim.cmd "colorscheme gruvbox"
+-- vim.cmd "highlight Normal guibg=none ctermbg=none"
+-- vim.cmd "highlight EndOfBuffer guibg=none ctermbg=none"
+-- vim.cmd "highlight StatusLine ctermbg=none cterm=bold"
---
+++ lua/user/options.lua
@@ -0,0 +1,21 @@
+ vim.opt.relativenumber = true
+ vim.opt.backspace = indent,eol,start
+ vim.opt.syntax = on
+ vim.opt.showmatch = true
+ vim.opt.ignorecase = true
+ vim.opt.hlsearch = true
+ vim.opt.incsearch = true
+ vim.opt.tabstop = 4
+ vim.opt.shiftwidth = 4
+ vim.opt.autoindent = true
+ vim.opt.number = true
+ vim.opt.background = dark
+ vim.opt.termguicolors = true
+ vim.opt.mouse = v
+ vim.opt.mouse = a
+ vim.opt.guicursor = "n-v-c-i:block"
+ vim.opt.guicursor = "a:blinkon100"
+
+ --nvim-tree
+ vim.g.loaded_netrw = 1
+ vim.g.loaded_netrwPlugin = 1
